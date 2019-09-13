ecedeab439e5ee7a3b922f544163fb43d16e3a7a
PolarNorth <antonov9312@yandex.ru>
2019-09-13 14:34:05 +0300

    Updated test file

--- test-files/test1-lex.scala
+++ test-files/test1-lex.scala
@@ -1,3 +1,5 @@
+// Taken from https://github.com/prisma/prisma/blob/master/server/servers/api/src/main/scala/com/prisma/api/server/QueryExecutor.scala
+
 package com.prisma.api.server
 
 import com.prisma.api.ApiDependencies
@@ -72,5 +74,59 @@ case class QueryExecutor()(implicit apiDependencies: ApiDependencies) {
   def errorExtractor(t: Throwable): Option[Int] = t match {
     case e: UserFacingError => Some(e.code)
     case _                  => None
+  }
+
+
+  def errorExtractor(t: Throwable): Option[Int] = t match {
+    case e: UserFacingError => Some(e.code)
+    case _                  => None
+  }
+}
+
+
+
+
+
+
+
+
+
+// Taken from https://gist.github.com/ricardclau/93a45952bb423046b06b
+
+package calculator
+
+object TweetLength {
+  final val MaxTweetLength = 140
+
+  def tweetRemainingCharsCount(tweetText: Signal[String]): Signal[Int] = {
+    Signal(MaxTweetLength - tweetLength(tweetText()))
+  }
+
+  def colorForRemainingCharsCount(remainingCharsCount: Signal[Int]): Signal[String] = {
+    Signal[String] (
+      remainingCharsCount() match {
+        case x if x >= 15 => "green"
+        case x if x >= 0 && x < 15 => "orange"
+        case _ => "red"
+      }
+    )
+  }
+
+  /** Computes the length of a tweet, given its text string.
+   *  This is not equivalent to text.length, as tweet lengths count the number
+   *  of Unicode *code points* in the string.
+   *  Note that this is still a simplified view of the reality. Full details
+   *  can be found at
+   *  https://dev.twitter.com/overview/api/counting-characters
+   */
+  private def tweetLength(text: String): Int = {
+    /* This should be simply text.codePointCount(0, text.length), but it
+     * is not implemented in Scala.js 0.6.2.
+     */
+    if (text.isEmpty) 0
+    else {
+      text.length - text.init.zip(text.tail).count(
+          (Character.isSurrogatePair _).tupled)
+    }
   }
 }
